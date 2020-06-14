4e945294f821391170f8ad9c008cd68a0544eac4
Clark Kent with glasses <61812163+locutusOO1@users.noreply.github.com>
2020-06-14 17:38:37 -0500

    current day details good, pulling needed forecast data

--- assets/code/code.js
+++ assets/code/code.js
@@ -2,12 +2,17 @@ $(document).ready(function() {
     // global variables
     var txtIn = "";
     var cityWeather = "http://api.openweathermap.org/data/2.5/weather?q=";
-    // html elements
+    // current day html elements
     var cityMain = $('#city-main');
+    var mainIcon = $('#main-icon');
     var cityTemp = $('#city-temp');
     var cityHumidity = $('#city-humidity');
     var cityWind = $('#city-wind');
     var cityUV = $('#city-uv');
+    // forcast array
+    var forecastIdx = 0;
+    // forcast html elements
+
 
     // get city weather api call
     function getWeather() {
@@ -18,29 +23,42 @@ $(document).ready(function() {
         $.getJSON(cityWeather)
             .done(function(response) {
                 console.log(response);
-                
+                var curDate = new Date(response.dt*1000).toLocaleDateString('en-US');
                 console.log(response.name);
-                cityMain.text(response.name);
+                cityMain.text(response.name + " (" + curDate + ") ");
+                mainIcon.attr("src","http://openweathermap.org/img/w/"+response.weather[0].icon+".png")
+                mainIcon.removeClass("hide");
 
                 console.log(response.main.temp);
-                cityTemp.text(response.main.temp);
+                cityTemp.text(response.main.temp+" "+String.fromCharCode(176)+"F");
 
                 console.log(response.main.humidity);
                 cityHumidity.text(response.main.humidity+"%");
 
                 console.log(response.wind.speed);
-                cityWind.text(response.wind.speed+"MPH");
+                cityWind.text(response.wind.speed+" MPH");
 
                 console.log(response.coord.lat);
                 console.log(response.coord.lon);
                 // get uv
-                var cityUV = "http://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&units=imperial&APPID=" + weatherKey;
-                console.log("UV: "+cityUV);
-                $.getJSON(cityUV)
+                var cityUVI = "http://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&units=imperial&APPID=" + weatherKey;
+                console.log("UV: "+cityUVI);
+                $.getJSON(cityUVI)
                     .done(function(json) {
                         console.log("uv response:");
                         console.log(json);
                         cityUV.text(json.value);
+                        if (json.value < 3) {
+                            cityUV.css("background-color","green");
+                        } else if (json.value < 6) {
+                            cityUV.css("background-color","yellow");
+                        } else if (json.value < 8) {
+                            cityUV.css("background-color","orange");
+                        } else if (json.value < 11) {
+                            cityUV.css("background-color","red");
+                        } else {
+                            cityUV.css("background-color","violet");
+                        }
                     })
                     .fail(function(error) {
                         console.log("uv error:");
@@ -49,9 +67,19 @@ $(document).ready(function() {
                 // get forecast
                 var cityFore = "http://api.openweathermap.org/data/2.5/forecast?q=" + txtIn + "&units=imperial&APPID=" + weatherKey;
                 console.log("forcast: "+cityFore);
-                $.getJSON(cityFore,function(response){
+                $.getJSON(cityFore,function(days){
                     console.log("forcast response:");
-                    console.log(response);
+                    console.log(days);
+                    forecastIdx = 0;
+                    for (var i = 0; i < days.list.length; i++) {
+                        if (days.list[i].dt_txt.indexOf(" 15:00:00") > 0) {
+                            console.log(days.list[i].dt_txt);
+                            console.log(days.list[i].dt);
+                            console.log(days.list[i].main.temp);
+                            console.log(days.list[i].main.humidity);
+                            console.log(days.list[i].weather[0].icon);
+                        }
+                    }
                 });
             })
             .fail(function(error) {
